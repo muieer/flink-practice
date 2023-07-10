8913ff345a295761d14ebe2555b7d0188bd08e2a
muieer <yang_for_work@qq.com>
2023-07-10 11:37:23 +0800

    init

---
+++ src/main/java/org/muieer/flink_practice/java/Main.java
@@ -0,0 +1,8 @@
+package org.muieer.flink_practice.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/scala/org/muieer/flink_practice/scala/Main.scala
@@ -0,0 +1,9 @@
+package org.muieer.flink_practice.scala
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
