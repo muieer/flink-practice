1c0d0064154e353a82b75628ac2378adb84e709f
muieer <yang_for_work@qq.com>
2023-07-30 14:31:22 +0800

    up

--- src/main/java/org/muieer/flink_practice/java/state/CountWindowAverage.java
+++ src/main/java/org/muieer/flink_practice/java/state/CountWindowAverage.java
@@ -1,8 +1,10 @@
 package org.muieer.flink_practice.java.state;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -16,15 +18,30 @@ public class CountWindowAverage extends RichFlatMapFunction<Tuple2<Long, Long>, 
     @Override
     public void open(Configuration parameters) throws Exception {
         super.open(parameters);
+
+        StateTtlConfig ttlConfig = StateTtlConfig
+                .newBuilder(Time.seconds(1))
+                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+                .build();
+
         ValueStateDescriptor<Tuple2<Long, Long>> descriptor =
-                new ValueStateDescriptor<>("average", TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {}),
-                    Tuple2.of(0L, 0L));
+                new ValueStateDescriptor<>("average", TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {}));
+        descriptor.enableTimeToLive(ttlConfig);
+
         sum = getRuntimeContext().getState(descriptor);
     }
 
+    // 同 key 每遇到两个数就聚合一次
     @Override
     public void flatMap(Tuple2<Long, Long> input, Collector<Tuple2<Long, Long>> out) throws Exception {
+
         // access the state value
+        if (sum.value() == null) {
+            sum.update(new Tuple2<>(input.f0, input.f1));
+            return;
+        }
+
         Tuple2<Long, Long> currentSum = sum.value();
 
         // update the count
