e348f256001ebdb83b6f68b475c38de61e728210
muieer <yang_for_work@qq.com>
2024-01-12 16:57:39 +0800

    up

--- src/main/java/org/muieer/flink_practice/java/state/EnableCheckpointing.java
+++ src/main/java/org/muieer/flink_practice/java/state/EnableCheckpointing.java
@@ -13,6 +13,12 @@ public class EnableCheckpointing {
 
 //        env.enableCheckpointing(1000);
 
+        /*
+        * CheckpointInterval 和 MinPauseBetweenCheckpoints 之间的关联
+        * 假设执行一次 cp 的时间是 t，上一次开始执行 cp 的时间是 c
+        * if t + MinPauseBetweenCheckpoints < CheckpointInterval，下一次执行 cp 的时间是 c + CheckpointInterval
+        * else 下一次执行 cp 的时间是 c + t + MinPauseBetweenCheckpoints
+        * */
         CheckpointConfig config = env.getCheckpointConfig();
         config.setCheckpointingMode(CheckpointingMode.AT_LEAST_ONCE);
         // 周期性执行 cp 的等待时长
