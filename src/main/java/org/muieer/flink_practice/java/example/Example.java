a3b3e0e3e6bf1b0395c7476e5b52bd48882f0324
muieer <yang_for_work@qq.com>
2024-07-07 15:53:45 +0800

    update org.muieer.flink_practice.java.example.Example

--- src/main/java/org/muieer/flink_practice/java/example/Example.java
+++ src/main/java/org/muieer/flink_practice/java/example/Example.java
@@ -1,35 +1,16 @@
 package org.muieer.flink_practice.java.example;
 
-import lombok.*;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.datastream.DataStream;
 
 public class Example {
 
     public static void main(String[] args) throws Exception {
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStream<Person> dataStream = env.fromElements(
-                new Person("Fred", 35),
-                new Person("muieer", 26),
-                new Person("Pebbles", 2)
-        );
 
-        SingleOutputStreamOperator<Person>
-                filterOperator = dataStream.filter(person -> person.age >= 18);
-        filterOperator.print();
+        env.setBufferTimeout(10000);
+        env.socketTextStream("localhost", 9999).print();
 
         env.execute();
-    }
-
-    @Getter
-    @Setter
-    @ToString
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Person {
-        private String name;
-        private Integer age;
     }
 }
